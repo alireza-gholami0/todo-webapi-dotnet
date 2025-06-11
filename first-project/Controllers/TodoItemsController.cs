c90a6bcdbb8eccaaad72be11e867245b66e901a2
alireza gholami <gholamialireza1004@gmail.com>
2025-06-11 19:56:38 +0330

    Add TodoItemsController

---
+++ first-project/Controllers/TodoItemsController.cs
@@ -0,0 +1,17 @@
+﻿using first_project.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace first_project.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TodoItemsController : ControllerBase
+{
+    private readonly TodoContext _context;
+
+    public TodoItemsController(TodoContext context)
+    {
+        _context = context;
+    }
+}
+
