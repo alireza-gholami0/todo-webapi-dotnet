f8db1a90084909608097842d9a94c18a37193dfc
alireza gholami <gholamialireza1004@gmail.com>
2025-06-11 18:39:14 +0330

    add TodoItem class

---
+++ first-project/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace first_project.Models
+{
+    public class TodoItem
+    {
+        int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public bool IsComplete { get; set; }
+    }
+}
